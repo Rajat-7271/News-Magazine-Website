b805e4f0e615e9f848e70849c2efe7fad696ee5d
Rajat <rajatbhakte412@gmail.com>
2025-07-30 00:36:39 +0530

    Initial commit - NewsMag basic version

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({ category }) => {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true); // Start loading
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setArticles(data.articles);
+                setLoading(false); // Stop loading after data fetched
+            });
+    }, [category]);
+
+    return (
+        <div>
+            <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+            {loading ? (
+                <h4 className="text-center">Loading...</h4>
+            ) : (
+                articles.map((news, index) => (
+                    <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+                ))
+            )}
+        </div>
+    );
+};
+
+export default NewsBoard;
---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,23 @@
+import defaultImage from '../assets/news.jpg';
+
+const NewsItem = ({ title, description, src, url }) => {
+    return (
+        <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3" style={{ maxWidth: "345px" }}>
+            <img
+                src={src ? src : defaultImage}
+                className="card-img-top img-fluid"
+                style={{ height: "200px", objectFit: "cover" }}
+                alt="News"
+            />
+            <div className="card-body">
+                <h5 className="card-title">{title ? (title.length > 50 ? `${title.slice(0, 50)}...` : title) : "Untitled News"}</h5>
+                <p className="card-text">
+                    {description ? (description.length > 90 ? `${description.slice(0, 90)}...` : description) : "No description available."}
+                </p>
+                <a href={url} className="btn btn-primary" target="_blank" rel="noreferrer">Read More</a>
+            </div>
+        </div>
+    )
+}
+
+export default NewsItem;
